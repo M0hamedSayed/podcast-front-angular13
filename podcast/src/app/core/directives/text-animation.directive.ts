a8057f160e1f7d2460562fa511853030b11f2c14
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-03-14 08:14:39 +0200

    text animation directive with gsap
    need to pass color for text only

---
+++ podcast/src/app/core/directives/text-animation.directive.ts
@@ -0,0 +1,47 @@
+import { Directive, Input, ElementRef, AfterViewInit } from '@angular/core';
+import gsap from 'gsap';
+
+@Directive({
+  selector: '[appTextAnimation]'
+})
+export class TextAnimationDirective implements AfterViewInit {
+  @Input('appTextAnimation') attrCSS!: string;
+  constructor(public el: ElementRef) { }
+  ngAfterViewInit(): void {
+    this.animationText()
+
+
+  }
+  animationText() {
+    let chars: Element = this.el.nativeElement;
+    let content = chars.textContent?.toLowerCase();
+
+    document.styleSheets[0].insertRule(
+      `.${content}[ng-reflect-attr-c-s-s]::before {
+        content:'${chars.textContent}';
+        position: absolute;
+        left: 0;
+        top: 0;
+        transform: translateY(-110%);
+        color: transparent;
+        background-clip: text;
+        --webkit--background-clip: text;
+        -webkit-text-stroke: 5px ${this.attrCSS};
+      }`, 0)
+    let timeLine = gsap.timeline();
+    timeLine.to(chars, {
+      duration: 3,
+      yPercent: 0,
+      stagger: 0.05,
+      ease: "expo.inOut"
+    }).to(chars, {
+      duration: 3,
+      yPercent: 110,
+      ease: "expo.inOut",
+      repeat: -1,
+      yoyo: true,
+      stagger: 0.1
+    })
+
+  }
+}
